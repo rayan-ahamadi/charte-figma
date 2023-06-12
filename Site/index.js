88b38d1df39ec2f7a13ec87118748b0f9892924a
rayan-ahamadi <115161695+rayan-ahamadi@users.noreply.github.com>
2023-06-12 02:52:46 +0200

    Ajout du code du site sans responsive

---
+++ Site/index.js
@@ -0,0 +1,21 @@
+function plateforme() {
+    var title = document.getElementById("title-cursus");
+    var paragraphe = document.getElementById("texte-cursus");
+    paragraphe.innerHTML = "Après mon retour à Marseille, j'ai décidé de refaire une formation en informatique. On m'a recommandé une formation gratuite en informatique : Le Bachelor IT à La Plateforme. <br>La première année se fait à 100% en présentiel. On apprend les bases de l'informatique avec une pédagogie en mode projet. Qui nous permet d'apprendre en allant chercher les informations sois-même. ";
+    title.innerHTML = "Le Bachelor IT : La Plateforme. 2022 - Maintenant";
+}
+
+function bellepierre() {
+    var title = document.getElementById("title-cursus");
+    var paragraphe = document.getElementById("texte-cursus");
+    paragraphe.innerHTML = "Après l'obtention du Bac, j'ai décidé de partir vivre à la réunion pour rejoindre une partie de ma famille et commencé mes études supérieurs à leurs côtés. J'ai donc était accepté au Lycée Bellepierre grâce à parcoursup. <br>Le premier semestre j'ai appris des bases en réseau informatique et en programmation web. Puis au deuxième semestre j'ai choisi l'option SLAM (Solution logiciel application métier), qui m'a permis d'en apprendre plus sur la programmation logiciel (notamment en Visual Basic)";
+    title.innerHTML = "Le BTS SIO : Lycée Bellepierre. 2021 - 2022";
+  }
+
+  function chatelier() {
+    var title = document.getElementById("title-cursus");
+    var paragraphe = document.getElementById("texte-cursus");
+    paragraphe.innerHTML = "Après le collège, j'ai décidé de continuer l'école avec un Bac Professionel en Système Numérique, car j'étais déjà passionnées par les nouvelles technologies et l'informatique. <br>J'ai pû apprendre les bases des systèmes d'exploitation et des réseaux informatiques. C'est d'ailleurs pendant mes années au lycée que j'ai pû m'intéresser à la programmation, ce qui m'a pousser à apprendre des bases en autodidacte car la programmation informatique n'était pas dans le cursus.";
+    title.innerHTML = "Le BAC PRO SN : Lycée Le Chatelier. 2018-2021";
+  }
+
